57ed0b95f7933c1dd520f81f5285b2aa19d0d4a5
MIMIIK <injan9949@gmail.com>
2024-10-08 09:42:35 +0545

    Homepage is updated with the css and some hero content

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,21 +1,41 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 import HeroSection from '../components/HeroSection';
 import '../styles/Images.css';
+import '../styles/Homepage.css';
 
-const Home = ()=> {
-    useEffect(()=>{
+const Home = () => {
+    useEffect(() => {
         document.title = 'DHOLIMARA';
     }, []);
-    return(
+    return (
         <div>
             <Navbar />
             <HeroSection />
             <img src='/homepage1.jpg' className='responsive-image' />
-            Welcome To DHOLIMARA
-            <Footer />
+            <div className='home-herosection'>
+                <p className='slogan'>Discover the Tranquility Within our Village</p>
+                <p className='hero-content'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor assumenda vel fugit, accusamus voluptatem, similique harum laborum dicta ipsa sapiente pariatur id! Obcaecati error asperiores totam ullam, debitis voluptatem provident.
+                </p>
+                <button className='learn-more'> <i class="fa-solid fa-lightbulb"></i>  Learn More</button>
             </div>
+            <div className='about-us'>
+                <div className='about-us-content'>
+                    <p className='slogan-about'>About Dholimara</p>
+                    <p className='slogan-title-about'>A Paradise Village</p>
+                    <p className='about-content'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta nisi a quo perspiciatis deleniti necessitatibus voluptates! Ducimus nostrum doloremque ea voluptatibus, dolorum esse sit blanditiis fuga aut ad tempora cupiditate.
+                    </p>
+                    <Link to='/about'>
+                        <button className='learn-more-about'> <i class="fa-solid fa-lightbulb"></i>  Learn More</button>
+                    </Link>
+                </div>
+            </div>
+            <Footer />
+        </div>
     )
 }
 
